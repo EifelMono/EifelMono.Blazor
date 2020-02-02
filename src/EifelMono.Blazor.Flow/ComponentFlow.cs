26da50c76eb684b08a0eac8730c05fed8adab339
Andreas Klapperich <andreas@klapperich.de>
2020-02-02 16:02:05 +0100

    ICHG: More Log

--- src/EifelMono.Blazor.Flow/ComponentFlow.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Components;
-
-namespace EifelMono.Blazor.Flow
-{
-    public class ComponentFlow : ComponentBase
-    {
-        public string ErrorMessage { get; set; }
-    }
-}
