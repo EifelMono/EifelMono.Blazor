00b4759fbcec34e990e040edf4ca4220fd7fe9bd
Andreas Klapperich <andreas@klapperich.de>
2020-02-08 12:33:44 +0100

    ADD: IfAny

---
+++ src/EifelMono.Blazor.Flow/Flow.If/IfAny.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Components;
+
+namespace EifelMono.Blazor.Flow
+{
+    public class IfAny<T> : Base.IfBase
+    {
+        protected override bool Condition() => Value.Any();
+
+        [Parameter]
+        public IEnumerable<T> Value { get; set; }
+    }
+}
